a1b223cc135ff40a0085ba8285a893c5acb9faee
Vernon Crock <vmcrock@gmail.com>
2024-01-28 20:52:50 +1100

    changes to improve config

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from azure.identity import DefaultAzureCredential # pip install azure-identity
-from azure.mgmt.web import WebSiteManagementClient # pip install azure-mgmt-web
+import sys
+
+from azure.identity import DefaultAzureCredential # pip install azure-identity   # pip show azure-identity
+from azure.mgmt.web import WebSiteManagementClient # pip install azure-mgmt-web  # pip show azure-mgmt-web
+
+# from azure.mgmt.web.models import SiteAppSettings
 
 
 # Azure subscription ID
@@ -20,13 +24,54 @@ credential = DefaultAzureCredential()
 # Initialize the WebSite management client
 web_client = WebSiteManagementClient(credential, subscription_id)
 
+# Initialize the WebSite management client
+#web_client = WebSiteManagementClient(credential, subscription_id)
+
 # Retrieve the current application settings
-app_settings = web_client.web_apps.list_application_settings(resource_group_name, app_service_name).properties
+# app_settings = web_client.web_apps.list_application_settings(resource_group_name, app_service_name).properties
+
+
+# Retrieve the current application settings
+app_settings = web_client.web_apps.list_application_settings(resource_group_name, app_service_name)
+
+# Modify the settings
+app_settings.properties[setting_name] = setting_value
+
+print(app_settings)
+
+
+
+# Retrieve the current application settings
+# current_settings = web_client.web_apps.list_application_settings(resource_group_name, app_service_name)
+# app_settings = current_settings.properties
+
+# print(app_settings)
+
+# sys.exit(0)
+
 
 # Add/Edit the application setting
-app_settings[setting_name] = setting_value
+# app_settings[setting_name] = setting_value
+
+# Convert dictionary to SiteAppSettings object
+# new_settings = SiteAppSettings(properties=app_settings)
+
+
+# Add/Edit the application setting
+#app_settings[setting_name] = setting_value
 
 # Update the application settings
-web_client.web_apps.update_application_settings(resource_group_name, app_service_name, str_dict=app_settings)
+# web_client.web_apps.update_application_settings(resource_group_name, app_service_name, str_dict=app_settings)
+
+# Update the application settings
+# updated_settings = web_client.web_apps.update_application_settings(resource_group_name, app_service_name, app_settings)
+
+# Update the application settings
+# updated_settings = web_client.web_apps.update_application_settings(resource_group_name, app_service_name, app_settings=new_settings)
+
+# Update the application settings
+web_client.web_apps.update_application_settings(resource_group_name, app_service_name, app_settings)
+
+
 
 print(f"Application setting '{setting_name}' has been updated.")
